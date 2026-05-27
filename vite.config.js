79eb5055b40e27f54e4e632ca66cfe033bea42e6
jhonbeltran1979-glitch <jhonbeltran1979@gmail.com>
2026-05-26 20:38:21 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        // Forzar nombres únicos en cada build para evitar caché
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`
+      }
+    }
+  }
 })
